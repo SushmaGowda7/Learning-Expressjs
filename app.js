39a7476d305b306f1115a0f3bbe4475bb579a073
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2023-01-21 19:57:10 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactUsRoutes = require('./routes/contactUs');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(contactUsRoutes);
+app.use(successRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'views', 'success.html'))
+// })
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>'
-    )
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+router.post('/add-product', (req, res, next) => {
     res.redirect('/')
 })
 
-module.express = router;
+module.exports = router;
